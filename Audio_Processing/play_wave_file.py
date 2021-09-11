8b876f8bea6a4af9d6fb362ecfdf06c84a6db730
Kazuya Tsubokura <tsubokura21@gmail.com>
2021-09-11 12:49:15 +0900

    Update play_wave_file.py

--- Audio_Processing/play_wave_file.py
+++ Audio_Processing/play_wave_file.py
@@ -9,40 +9,43 @@ import pyaudio
 import wave
 import sys
 
-CHUNK = 1024
+def main(file_name):
+    CHUNK = 1024
 
-file_name = 'hoge.wav'
+    try:
+        wave_file = wave.open(file_name, 'rb')
+    except FileNotFoundError:
+        print('ファイル名が間違っているかも？')
+        exit()
 
-try:
-    wave_file = wave.open(file_name, 'rb')
-except FileNotFoundError:
-    print('ファイル名が間違っているかも？')
-    exit()
+    # wave fileの基本情報
+    print('channnle:', wave_file.getnchannels())
+    print('frame rate:', wave_file.getframerate(), '[Hz]')
 
-# wave fileの基本情報
-print('channnle:', wave_file.getnchannels())
-print('frame rate:', wave_file.getframerate(), '[Hz]')
+    # pyaudioのインスタンス
+    p = pyaudio.PyAudio()
 
-# pyaudioのインスタンス
-p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(format=p.get_format_from_width(wave_file.getsampwidth()),
+                    channels=wave_file.getnchannels(),
+                    rate=wave_file.getframerate(),
+                    output=True)
 
-# open stream
-stream = p.open(format=p.get_format_from_width(wave_file.getsampwidth()),
-                channels=wave_file.getnchannels(),
-                rate=wave_file.getframerate(),
-                output=True)
-
-# read data
-data = wave_file.readframes(CHUNK)
-
-# play stream
-while len(data) > 0:
-    stream.write(data)
+    # read data
     data = wave_file.readframes(CHUNK)
 
-# stop stream
-stream.stop_stream()
-stream.close()
+    # play stream
+    while len(data) > 0:
+        stream.write(data)
+        data = wave_file.readframes(CHUNK)
 
-# close PyAudio
-p.terminate()
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+
+    # close PyAudio
+    p.terminate()
+
+if __name__ == '__main__':
+    file_name = 'hoge.wav'
+    main(file_name)
