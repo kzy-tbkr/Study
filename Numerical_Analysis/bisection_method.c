12d44d51a06b69d51d13f6d701cf5a4fa26d6805
Kazuya Tsubokura <tsubokura21@gmail.com>
2021-09-17 22:05:18 +0900

    Update bisection_method.c

--- Numerical_Analysis/bisection_method.c
+++ Numerical_Analysis/bisection_method.c
@@ -8,17 +8,18 @@
 #define EPS 0.0001 // 収束判定条件
 
 double f(double x) {
-  // 5x^3 21x^2 -21x +5 = 0の解
+  // 5x^3 -21x^2 -21x +5 = 0の解
   return 5*x*x*x-21*x*x-21*x+5;
 }
 
 int main() {
+  // 0 <= x <= 1の範囲で探索
   double a = 0.0;
   double b = 1.0;
   double c;
 
   int cnt = 0; //繰り返し回数カウント
-  
+
   do {
     cnt++;
 
@@ -32,5 +33,6 @@ int main() {
 
   } while(fabsf(a-b) > EPS);
 
+  // 解出力
   printf("x=%f (count:%d)\n", c, cnt);
 }
