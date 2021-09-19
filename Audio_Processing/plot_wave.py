39feefd78a9811a84bb9ca0504da75e6f33265f3
Kazuya Tsubokura <tsubokura21@gmail.com>
2021-09-19 22:56:25 +0900

    Update plot_wave.py

--- Audio_Processing/plot_wave.py
+++ Audio_Processing/plot_wave.py
@@ -4,12 +4,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-point_per_sec = 1000
-sec = 1
-f = 10
+def make_sin(f, sec): # f:周波数(Hz), sec:時間(秒)
+    point_per_sec = 1000 # 1秒あたりのサンプル数
+    x = np.array([float(i/point_per_sec) for i in range(point_per_sec*sec)])
+    sin_x = np.sin(2*np.pi*f*x)
 
-x = np.array([float(i/point_per_sec) for i in range(point_per_sec*sec)])
-sin = np.sin(2*np.pi*f*x)
+    return x, sin_x
 
-plt.plot(x, sin)
-plt.show()
+if __name__ == '__main__':
+    f = 10
+    sec = 1
+    x, sin_x = make_sin(f, sec)
+    plt.plot(x, sin_x)
+    plt.show()
