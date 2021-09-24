342a87b8008333d2fa50c8fa51696e4251cf84de
Kazuya Tsubokura <tsubokura21@gmail.com>
2021-09-24 21:59:52 +0900

    Update newton_method.c

--- Numerical_Analysis/newton_method.c
+++ Numerical_Analysis/newton_method.c
@@ -7,22 +7,26 @@
 
 #define EPS 0.0001 // 収束判定条件
 
+// 元の関数
 double f(double x) {
   return x*x-6*x+5;
 }
 
+// 元の関数を微分したもの
 double df(double x) {
   return 2*x-6;
 }
 
 int main() {
-  double a = 10.0;
+  int cnt = 1; // 繰り返しカウント用
+  double a = 10.0; // 初期値
   double b = a - f(a) / df(a);
 
   while(fabsf(a-b) > EPS) {
     a = b;
     b = a - f(a) / df(a);
+    cnt++;
   }
 
-  printf("x=%f\n", b);
+  printf("x=%f (count:%d)\n", b, cnt);
 }
